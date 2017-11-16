13f76c681f2b711fa8ec8cd381f05e696d4c3735
Michael Kargl <michael.kargl@ohsnaparts.at>
2017-11-16 13:58:34 +0100

    - New nuget package reference (json.net) - Adds Star Model class

---
+++ RedTubeAPI.NET.Repository/Model/Star.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace RedTubeAPI.NET.Repository
+{
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class Star
+    {
+        [JsonProperty("star_name")]
+        public string Name { get; set; }
+
+        [JsonProperty("star_url")]
+        public string WebProfile { get; set; }
+
+        [JsonProperty("star_thumb")]
+        public string Thumbnail { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format(
+                "[Star: Name={0}, WebProfile={1}, Thumbnail={2}]",
+                Name, WebProfile, Thumbnail
+            );
+        }
+    }
+}
