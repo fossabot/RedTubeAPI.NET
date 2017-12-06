f6251ce8edb13e21b37698f8564764c75403f7b2
Michael Kargl <michael.kargl@ohsnaparts.at>
2017-12-06 17:03:45 +0100

    Adds new Unit Testing Project

---
+++ RTARepositoryRequest/RTARepoRequest.Test/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RTARepoRequest.Test
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ RTARepositoryRequest/RTARepositoryRequest/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RTARepositoryRequest
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
